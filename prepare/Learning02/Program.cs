0769700d4bf94ccc103c135304423b23ff0280b7
lamoreauxrl <lamondlamoreaux@gmail.com>
2023-11-06 22:48:53 -0700

    wk2

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        
+        Job job1 = new Job();
+        job1._company = "Rainfocus";
+        job1._jobTitle = "Junior Logistics and Shipping";
+        job1._startYear = 2023;
+        job1._endYear = 2023;
+        
+
+        Job job2 = new Job();
+        job2._company = "Mesa Solutions";
+        job2._jobTitle = "Warehouse Manager";
+        job2._startYear = 2021;
+        job2._endYear = 2021;
+
+        Resume myResume = new Resume();
+        myResume._name = "LaMond Lamoreaux";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+        myResume.DisplayResume();
+        
     }
+
+    
+
 }
