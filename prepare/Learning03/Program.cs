bcaa1989f74cd3a95deedf0c76b4b98652a5902f
lamoreauxrl <lamondlamoreaux@gmail.com>
2023-11-16 20:44:56 -0700

    ....

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -8,6 +8,12 @@ class Program
         Fraction fraction1 = new Fraction(3);
         Fraction fraction2 = new Fraction(8, 11);
 
+        Console.WriteLine(fraction.GetBottom());
+        Console.WriteLine(fraction1.GetTop());
+        Console.WriteLine(fraction2.GetTop());
+        Console.WriteLine(fraction2.GetBottom());
+        Console.WriteLine(fraction.GetFractionString());
+        Console.WriteLine(fraction1.GetDecimalValue());
         Console.WriteLine(fraction2.GetFractionString());
         Console.WriteLine(fraction2.GetDecimalValue());
     }
